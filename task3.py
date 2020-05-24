414eb57555ff8e502c12213b29eb3774f2606c3f
souvik <souvikdutta7012@gmail.com>
2020-05-24 17:48:37 +0530

    v7

--- task3.py
+++ task3.py
@@ -1,6 +1,6 @@
 import keras
 from keras.models import Sequential
 from keras.layers import Dense,Flatten,Activation
-def result(accuracy):
-	return accuracy
-result(80)
+accuracy=80
+f=open("report.txt","w")
+f.write(str(accuracy))
