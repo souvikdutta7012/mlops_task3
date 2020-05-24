924ff681481d4bc3ffdf7d3668abf7532c9c2d47
souvik <souvikdutta7012@gmail.com>
2020-05-24 10:50:29 +0530

    v3

--- task3.py
+++ task3.py
@@ -1,5 +1,5 @@
-
-from keras.model import Sequential
+import keras
+from keras.models import Sequential
 from keras.layers import Dense,Flatten,Activation
 from keras.layers import Conv2D,MaxPolling2D
 print("Successful")
