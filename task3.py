de60506f27bc3be79df6275d1060f140446dfbb1
souvik <souvikdutta7012@gmail.com>
2020-05-24 15:40:15 +0530

    v6

--- task3.py
+++ task3.py
@@ -1,5 +1,6 @@
 import keras
 from keras.models import Sequential
 from keras.layers import Dense,Flatten,Activation
-accuracy = 80
-return accuracy
+def result(accuracy):
+	return accuracy
+result(80)
