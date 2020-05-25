2723fc7bab8ae39c22fc031a0d2320f478efb8f6
Souvik Dutta <64468652+souvikdutta7012@users.noreply.github.com>
2020-05-26 00:19:41 +0530

    v1

--- task3.py
+++ task3.py
@@ -1,6 +1,72 @@
+
 import keras
 from keras.models import Sequential
-from keras.layers import Dense,Flatten,Activation
-accuracy=90
-f=open("report.txt","w")
-f.write(str(accuracy))
+from keras.layers import Dense,Flatten,MaxPooling2D,Convolution2D
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import Adam
+import smtplib, ssl
+
+
+model=Sequential()
+model.add(Convolution2D(filters=32,kernel_size=(3,3),activation='relu',input_shape=(64,64,3)))
+model.add(MaxPooling2D())
+model.add(Flatten())
+model.add(Dense(units=128,activation='relu'))
+model.add(Dense(units=1,activation='sigmoid'))
+model.summary()
+model.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
+
+
+f=open("requirement.txt","r+")
+epoch=int((f.readline())
+f.close()
+
+
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+test_datagen = ImageDataGenerator(rescale=1./255)
+train_generator = train_datagen.flow_from_directory(
+        '/mlops/task3/animals/valid/',
+        target_size=(64, 64),
+        batch_size=32,
+        class_mode='binary')
+validation_generator = test_datagen.flow_from_directory(
+        '/mlops/task3/animals/valid/',
+        target_size=(64, 64),
+        batch_size=32,
+        class_mode='binary')
+history=model.fit(
+        train_generator,
+        steps_per_epoch=1,
+        epochs=epoch,
+        validation_data=validation_generator,
+        validation_steps=800)
+
+
+
+
+l=[]
+l=history.history['accuracy']
+
+
+
+n=len(l)
+accuracy=l[n-1]
+if accuracy<0.8:
+    f1=open("report.txt","w")
+    f1.write(str(accuracy))
+    f1.close()
+
+    f2=open("requirement.txt","w")
+    epoch=int(epoch)+1
+    f2.write(str(epoch))
+    f2.close
+else:
+    model.save("model.h5")
+
+
+
+
